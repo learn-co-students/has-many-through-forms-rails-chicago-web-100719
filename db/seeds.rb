d1b45242d28605f0b09ed7e5b21a25f69f0ec846
Daniel Ionita <danielionita@Daniels-MacBook-Pro.local>
2019-11-09 14:00:31 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+funny = Category.create(name: "funny")
+politics = Category.create(name: "politics")
